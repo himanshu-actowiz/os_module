efe5ecea64439bde2858e6dfa99827255f818306
Hemanshu Marwadi <Hemanshu.Marwadi@act.local>
2026-02-13 11:38:30 +0530

    Implement function to generate folders between two dates

--- Make_DIR_Datewise.py
+++ Make_DIR_Datewise.py
@@ -3,12 +3,14 @@ import datetime
 
 def create_folders_between_dates():
     current_date = datetime.date.today()
-    last_date = datetime.date(2027,1,1)
+    last_date = datetime.date(2026,12,31)
     list_of_dates = (last_date-current_date).days #list of dates between two dates
 
 
     for i in range(list_of_dates):
         current_date += datetime.timedelta(days=1) #day's Incress to one by one
-        os.mkdir(f"C:/Users/hemanshu.marwadi/Desktop/Himanshu/{current_date}")
+        os.mkdir(f"C:/Users/hemanshu.marwadi/Desktop/Himanshu/OS_Module_Make_DIR/{current_date}")
           
 create_folders_between_dates()
+
+
