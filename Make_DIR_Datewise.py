88b804323167f8b18c418368ad40fcd7cd6aac70
Hemanshu Marwadi <Hemanshu.Marwadi@act.local>
2026-02-13 15:00:27 +0530

    generate remaining year folders with leap year support

--- Make_DIR_Datewise.py
+++ Make_DIR_Datewise.py
@@ -1,17 +1,23 @@
 import os
-from datetime import *
+from datetime import datetime, timedelta
+from calendar import isleap
 
 Current_date = input("Enter a CURRENT DATE : YYYY-MM-DD ")
 
-def create_folders_between_dates(Current_date):   
-    Convert_date_DATETIME = datetime.strptime(Current_date,"%Y-%m-%d")
+def create_folders_between_dates(Current_date):
+    Convert_date_DATETIME = datetime.strptime(Current_date, "%Y-%m-%d")
     Current_Day_Number = Convert_date_DATETIME.timetuple().tm_yday
-    Find_Remaning_Days = 365-Current_Day_Number + 1
-    for i in range(1,Find_Remaning_Days):
+    year = Convert_date_DATETIME.year # Get year correctly
+    total_days = 366 if isleap(year) else 365  # Check leap year properly
+    
+    Find_Remaining_Days = total_days - Current_Day_Number
+
+    for i in range(1, Find_Remaining_Days + 1):
         Increase_Date = Convert_date_DATETIME + timedelta(days=i)
-        s = str(Increase_Date).split(" ")[0]
+        s = Increase_Date.strftime("%Y-%m-%d")
+        
         os.mkdir(f"C:/Users/hemanshu.marwadi/Desktop/OS_MAKE_DIR/{s}")
+        
 
 create_folders_between_dates(Current_date)
-
 
