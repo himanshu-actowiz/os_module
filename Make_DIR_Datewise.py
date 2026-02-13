12078632061d14e1682ccb87414883f510dc2889
Hemanshu Marwadi <Hemanshu.Marwadi@act.local>
2026-02-13 14:01:56 +0530

    Implement folder generator based on user-provided date

--- Make_DIR_Datewise.py
+++ Make_DIR_Datewise.py
@@ -21,7 +21,7 @@ def create_folders_between_dates(Current_date):
     Convert_date_DATETIME = datetime.strptime(Current_date,"%Y-%m-%d")
     Current_Day_Number = Convert_date_DATETIME.timetuple().tm_yday
     Find_Remaning_Days = 365-Current_Day_Number + 1
-    for i in range(Find_Remaning_Days):
+    for i in range(1,Find_Remaning_Days):
         Increase_Date = Convert_date_DATETIME + timedelta(days=i)
         s = str(Increase_Date).split(" ")[0]
         os.mkdir(f"C:/Users/hemanshu.marwadi/Desktop/OS_MAKE_DIR/{s}")
